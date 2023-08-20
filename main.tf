a5990be68a6c0a0debd99178f26aca786f5a6dc2
nizarakbarm <nizar@domainesia.com>
2023-08-20 23:13:06 +0700

    fix: Fix wrong repository url at helm_release vault
    Change wrong repository url to https://helm.releases.hashicorp.com at helm_release vault

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "kubernetes_namespace" "createnamespacevault" {
 resource "helm_release" "vault" {
     name = "vault"
     namespace = "vault"
-    repository = "https://helm.releases.hashicorp"
+    repository = "https://helm.releases.hashicorp.com"
     chart = "vault"
 
     values = [
